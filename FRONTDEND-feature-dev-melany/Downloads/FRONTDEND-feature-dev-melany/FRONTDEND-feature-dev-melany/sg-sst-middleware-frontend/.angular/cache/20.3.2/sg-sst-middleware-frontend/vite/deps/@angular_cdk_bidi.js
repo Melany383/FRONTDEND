00ee14fdf67416aff4e61b126f197c8401b60c1b
Savama <140782510+vamaho@users.noreply.github.com>
2025-09-30 08:30:59 -0500

    Cambio Valentin -sidebar

---
+++ FRONTDEND-feature-dev-melany/Downloads/FRONTDEND-feature-dev-melany/FRONTDEND-feature-dev-melany/sg-sst-middleware-frontend/.angular/cache/20.3.2/sg-sst-middleware-frontend/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-V2RJEA2R.js";
+import "./chunk-CFGOWU7P.js";
+import "./chunk-HWYXSU2G.js";
+import "./chunk-JRFR6BLO.js";
+import "./chunk-MARUHEWW.js";
+import "./chunk-TXDUYLVM.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
